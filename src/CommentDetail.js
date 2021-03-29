6019de29232bfc8cdd4c6cbcbd05378f77990bc3
theresfuelinmycoffee <atigrouse.com>
2021-03-28 22:30:31 -0700

    added custome card component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
+import faker from "faker";
 
 const CommentDetail = (props) => {
-    return(
-        <div className = "comment" >
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ props.profilePic } />
-                </a>
-                <div className="content">
-                  <a herf="/" className="author">
-                    {props.author}
-                  </a>
-                  <div className="metadata">
-                      <span className="date">{ props.timeAgo }</span>
-                  </div>
-                  <div className="text">{ props.postComment } </div>
-                </div>
-            </div >
-    );
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.profilePic} />
+      </a>
+      <div className="content">
+        <a herf="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
+        </div>
+        <div className="text">{props.postComment} </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
 
 const App = () => {
-    return (
-        <div className="ui container comments">   
-        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" postComment="Nice!" profilePic = {faker.image.avatar()} />
-        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" postComment="Sweet Blog!" profilePic = {faker.image.avatar()} />
-        <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" postComment="Awesome!" profilePic = {faker.image.avatar()} />
-        </div>
-        
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        postComment="Nice!"
+        profilePic={faker.image.avatar()}
+      />
+      </ApprovalCard>
+
+      <ApprovalCard>
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        postComment="Sweet Blog!"
+        profilePic={faker.image.avatar()}
+      />
+      </ApprovalCard>
+
+      <ApprovalCard>
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        postComment="Awesome!"
+        profilePic={faker.image.avatar()}
+      />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
