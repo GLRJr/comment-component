53af50d0e1a68488bed1bdbc22f2900ac81e50d5
theresfuelinmycoffee <atigrouse.com>
2021-03-28 21:35:10 -0700

    added new component props

--- src/index.js
+++ src/index.js
@@ -1,52 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() } />
-                </a>
-                <div className="content">
-                  <a herf="/" className="author">
-                      Sam
-                  </a>
-                  <div className="metadata">
-                      <span className="date">Today at 6:00PM</span>
-                  </div>
-                  <div className="text">Nice Blog Post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() } />
-                </a>
-                <div className="content">
-                  <a herf="/" className="author">
-                      Ashley
-                  </a>
-                  <div className="metadata">
-                      <span className="date">Today at 6:00PM</span>
-                  </div>
-                  <div className="text">Nice Blog Post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() } />
-                </a>
-                <div className="content">
-                  <a herf="/" className="author">
-                      Leslie
-                  </a>
-                  <div className="metadata">
-                      <span className="date">Today at 6:00PM</span>
-                  </div>
-                  <div className="text">Nice Blog Post!</div>
-                </div>
-            </div>
+        <div className="ui container comments">   
+        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" postComment="Nice!" profilePic = {faker.image.avatar()} />
+        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" postComment="Sweet Blog!" profilePic = {faker.image.avatar()} />
+        <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" postComment="Awesome!" profilePic = {faker.image.avatar()} />
         </div>
         
     );
